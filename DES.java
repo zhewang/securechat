f4993f51369163863396d60af89ebf11904ffb99
Zhe Wang <zwang.contact@gmail.com>
2015-10-22 22:00:35 -0700

    Update DES with given skeleton

--- DES.java
+++ DES.java
@@ -1,5 +1,154 @@
-class DES{
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import gnu.getopt.Getopt;
+
+
+public class DES {
+
+	public static void main(String[] args) {
+
+		StringBuilder inputFile = new StringBuilder();
+		StringBuilder outputFile = new StringBuilder();
+		StringBuilder keyStr = new StringBuilder();
+		StringBuilder encrypt = new StringBuilder();
+
+		pcl(args, inputFile, outputFile, keyStr, encrypt);
+
+		if(keyStr.toString() != "" && encrypt.toString().equals("e")){
+			encrypt(keyStr, inputFile, outputFile);
+		} else if(keyStr.toString() != "" && encrypt.toString().equals("d")){
+			decrypt(keyStr, inputFile, outputFile);
+		}
+
+
+	}
+
+
+	private static void decrypt(StringBuilder keyStr, StringBuilder inputFile,
+			StringBuilder outputFile) {
+		try {
+			PrintWriter writer = new PrintWriter(outputFile.toString(), "UTF-8");
+			List<String> lines = Files.readAllLines(Paths.get(inputFile.toString()), Charset.defaultCharset());
+			String IVStr = lines.get(0);
+			lines.remove(0);
+			String encryptedText;
+
+			for (String line : lines) {
+				encryptedText = DES_decrypt(IVStr, line);
+				writer.print(encryptedText);
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * TODO: You need to write the DES encryption here.
+	 * @param line
+	 */
+	private static String DES_decrypt(String iVStr, String line) {
+
+		return null;
+	}
+
+
+	private static void encrypt(StringBuilder keyStr, StringBuilder inputFile,
+			StringBuilder outputFile) {
+
+		try {
+			PrintWriter writer = new PrintWriter(outputFile.toString(), "UTF-8");
+
+			String encryptedText;
+			for (String line : Files.readAllLines(Paths.get(inputFile.toString()), Charset.defaultCharset())) {
+				encryptedText = DES_encrypt(line);
+				writer.print(encryptedText);
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+	/**
+	 * TODO: You need to write the DES encryption here.
+	 * @param line
+	 */
+	private static String DES_encrypt(String line) {
+
+		return null;
+	}
+
+
+	static void genDESkey(){
+		System.out.println("New key goes here");
+		return;
+	}
+
+
+	/**
+	 * This function Processes the Command Line Arguments.
+	 * -p for the port number you are using
+	 * -h for the host name of system
+	 */
+	private static void pcl(String[] args, StringBuilder inputFile,
+							StringBuilder outputFile, StringBuilder keyString,
+							StringBuilder encrypt) {
+		/*
+		 * http://www.urbanophile.com/arenn/hacking/getopt/gnu.getopt.Getopt.html
+		*/
+		Getopt g = new Getopt("Chat Program", args, "hke:d:i:o:");
+		int c;
+		String arg;
+		while ((c = g.getopt()) != -1){
+		     switch(c){
+		     	  case 'o':
+		        	  arg = g.getOptarg();
+		        	  outputFile.append(arg);
+		        	  break;
+		     	  case 'i':
+		        	  arg = g.getOptarg();
+		        	  inputFile.append(arg);
+		        	  break;
+	     	  	  case 'e':
+		        	  arg = g.getOptarg();
+		        	  keyString.append(arg);
+		        	  encrypt.append("e");
+		        	  break;
+	     	  	  case 'd':
+		        	  arg = g.getOptarg();
+		        	  keyString.append(arg);
+		        	  encrypt.append("d");
+		        	  break;
+		          case 'k':
+		        	  genDESkey();
+		        	  break;
+		          case 'h':
+		        	  callUseage(0);
+		          case '?':
+		            break; // getopt() already printed an error
+		            //
+		          default:
+		              break;
+		       }
+		   }
+
+	}
+
+	private static void callUseage(int exitStatus) {
+
+		String useage = "Print Usage";
+
+		System.err.println(useage);
+		System.exit(exitStatus);
+
+	}
+
 }
