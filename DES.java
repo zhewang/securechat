2f1adfdca7b586d6e5b6914c3a6113cf6730d4d3
Zhe Wang <zwang.contact@gmail.com>
2015-10-24 15:07:37 -0700

    Add IP and FP s-box

--- DES.java
+++ DES.java
@@ -9,7 +9,38 @@ import java.lang.Math;
 
 import gnu.getopt.Getopt;
 
+import SBoxes;
+
 public class DES {
+
+    /**
+     * S-Box for Initial Permutation
+     */
+	private static int[] IP = {
+		58, 50, 42, 34, 26, 18, 10, 2
+		, 60, 52, 44, 36, 28, 20, 12, 4
+		, 62, 54, 46, 38, 30, 22, 14, 6
+		, 64, 56, 48, 40, 32, 24, 16, 8
+		, 57, 49, 41, 33, 25, 17, 9, 1
+		, 59, 51, 43, 35, 27, 19, 11, 3
+		, 61, 53, 45, 37, 29, 21, 13, 5
+		, 63, 55, 47, 39, 31, 23, 15, 7
+	};
+
+    /**
+     * S-Box for Final Permutation
+     */
+	private static int[] FP = {
+		40, 8, 48, 16, 56, 24, 64, 32
+		, 39, 7, 47, 15, 55, 23, 63, 31
+		, 38, 6, 46, 14, 54, 22, 62, 30
+		, 37, 5, 45, 13, 53, 21, 61, 29
+		, 36, 4, 44, 12, 52, 20, 60, 28
+		, 35, 3, 43, 11, 51, 19, 59, 27
+		, 34, 2, 42, 10, 50, 18, 58, 26
+		, 33, 1, 41, 9, 49, 17, 57, 25
+	};
+
 	public static void main(String[] args) {
 
         StringBuilder inputFile = new StringBuilder();
@@ -24,8 +55,6 @@ public class DES {
         } else if(keyStr.toString() != "" && encrypt.toString().equals("d")){
             decrypt(keyStr, inputFile, outputFile);
         }
-
-
     }
 
     /*public static void main(String[] args){
@@ -115,9 +144,7 @@ public class DES {
                 i++;j++;
                 b >>= 1;
             }
-
         }
-
 
         return plainBlock;
     }
@@ -157,6 +184,7 @@ public class DES {
         cipher.append(BitSettoAscii(tmp2));
         return cipher.toString();
     }
+
     private static void encrypt(StringBuilder keyStr, StringBuilder inputFile,
             StringBuilder outputFile) {
 
