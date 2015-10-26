1683ee3d1b8f391f55022116e132a254c72ba92b
Zhe Wang <zwang.contact@gmail.com>
2015-10-25 19:37:57 -0700

    Code cleaning

--- DES.java
+++ DES.java
@@ -12,69 +12,66 @@ import java.math.BigInteger;
 
 import gnu.getopt.Getopt;
 
-// TODO: Implement key expansion
-// TODO: Implement genKeys
-
 
 public class DES {
 
-	/**
-	 * S-Box for Initial Permutation
-	 */
-	private static int[] IP = {
-		58, 50, 42, 34, 26, 18, 10, 2
-			, 60, 52, 44, 36, 28, 20, 12, 4
-			, 62, 54, 46, 38, 30, 22, 14, 6
-			, 64, 56, 48, 40, 32, 24, 16, 8
-			, 57, 49, 41, 33, 25, 17, 9, 1
-			, 59, 51, 43, 35, 27, 19, 11, 3
-			, 61, 53, 45, 37, 29, 21, 13, 5
-			, 63, 55, 47, 39, 31, 23, 15, 7
-	};
+    /**
+     * S-Box for Initial Permutation
+     */
+    private static int[] IP = {
+        58, 50, 42, 34, 26, 18, 10, 2
+        , 60, 52, 44, 36, 28, 20, 12, 4
+        , 62, 54, 46, 38, 30, 22, 14, 6
+        , 64, 56, 48, 40, 32, 24, 16, 8
+        , 57, 49, 41, 33, 25, 17, 9, 1
+        , 59, 51, 43, 35, 27, 19, 11, 3
+        , 61, 53, 45, 37, 29, 21, 13, 5
+        , 63, 55, 47, 39, 31, 23, 15, 7
+    };
 
-	/**
-	 * S-Box for Final Permutation
-	 */
-	private static int[] FP = {
-		40, 8, 48, 16, 56, 24, 64, 32
-			, 39, 7, 47, 15, 55, 23, 63, 31
-			, 38, 6, 46, 14, 54, 22, 62, 30
-			, 37, 5, 45, 13, 53, 21, 61, 29
-			, 36, 4, 44, 12, 52, 20, 60, 28
-			, 35, 3, 43, 11, 51, 19, 59, 27
-			, 34, 2, 42, 10, 50, 18, 58, 26
-			, 33, 1, 41, 9, 49, 17, 57, 25
-	};
-	/**
-	 * PC1 used in key expansion
-	 */
-	private static int[]  PC1 = {
-		57, 49, 41, 33, 25, 17, 9 ,
-		1 , 58, 50, 42, 34, 26, 18,
-		10, 2 , 59, 51, 43, 35, 27,
-		19, 11, 3 , 60, 52, 44, 36,
-		63, 55, 47, 39, 31, 23, 15,
-		7 , 62, 54, 46, 38, 30, 22,
-		14, 6 , 61, 53, 45, 37, 29,
-		21, 13, 5 , 28, 20, 12, 4 };
-	/**
-	 * PC2 used in key expansion
-	 */
-	private static int[] PC2 = {
-		14, 17, 11, 24, 1 , 5 ,
-		3 , 28, 15, 6 , 21, 10,
-		23, 19, 12, 4 , 26, 8 ,
-		16, 7 , 27, 20, 13, 2 ,
-		41, 52, 31, 37, 47, 55,
-		30, 40, 51, 45, 33, 48,
-		44, 49, 39, 56, 34, 53,
-		46, 42, 50, 36, 29, 32 };
-	/**
-	 * rotations used in key expansion
-	 */
-	private static int[] rotations = {
-		1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
-	};
+    /**
+     * S-Box for Final Permutation
+     */
+    private static int[] FP = {
+        40, 8, 48, 16, 56, 24, 64, 32
+        , 39, 7, 47, 15, 55, 23, 63, 31
+        , 38, 6, 46, 14, 54, 22, 62, 30
+        , 37, 5, 45, 13, 53, 21, 61, 29
+        , 36, 4, 44, 12, 52, 20, 60, 28
+        , 35, 3, 43, 11, 51, 19, 59, 27
+        , 34, 2, 42, 10, 50, 18, 58, 26
+        , 33, 1, 41, 9, 49, 17, 57, 25
+    };
+    /**
+     * PC1 used in key expansion
+     */
+    private static int[]  PC1 = {
+        57, 49, 41, 33, 25, 17, 9 ,
+        1 , 58, 50, 42, 34, 26, 18,
+        10, 2 , 59, 51, 43, 35, 27,
+        19, 11, 3 , 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+        7 , 62, 54, 46, 38, 30, 22,
+        14, 6 , 61, 53, 45, 37, 29,
+        21, 13, 5 , 28, 20, 12, 4 };
+    /**
+     * PC2 used in key expansion
+     */
+    private static int[] PC2 = {
+        14, 17, 11, 24, 1 , 5 ,
+        3 , 28, 15, 6 , 21, 10,
+        23, 19, 12, 4 , 26, 8 ,
+        16, 7 , 27, 20, 13, 2 ,
+        41, 52, 31, 37, 47, 55,
+        30, 40, 51, 45, 33, 48,
+        44, 49, 39, 56, 34, 53,
+        46, 42, 50, 36, 29, 32 };
+    /**
+     * rotations used in key expansion
+     */
+    private static int[] rotations = {
+        1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
+    };
 
     /**
      * f function: Expansion Permutation
@@ -93,7 +90,7 @@ public class DES {
     /**
      * f function: S-Box Substitution
      */
-	private static final byte[][] S = { {
+    private static final byte[][] S = { {
         14, 4,  13, 1,  2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0,  7,
         0,  15, 7,  4,  14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3,  8,
         4,  1,  14, 8,  13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5,  0,
@@ -138,15 +135,15 @@ public class DES {
     /**
      * f function: P-Box Substitution
      */
-	private static int[] P = {
-		16, 7, 20, 21,
-		29, 12, 28, 17,
-		1, 15, 23, 26,
-		5, 18, 31, 10,
-		2, 8, 24, 14,
-		32, 27, 3, 9,
-		19, 13, 30, 6,
-		22, 11, 4, 25};
+    private static int[] P = {
+        16, 7, 20, 21,
+        29, 12, 28, 17,
+        1, 15, 23, 26,
+        5, 18, 31, 10,
+        2, 8, 24, 14,
+        32, 27, 3, 9,
+        19, 13, 30, 6,
+        22, 11, 4, 25};
 
     public static void main(String[] args) {
 
@@ -402,57 +399,53 @@ public class DES {
         return cipher;
     }
 
-	/**
-	 * Generate key for each round based on encryption or decryption
-	 * @param keyStr The key string
-	 * @param type Encryption or Decryption. Values are 'e' or 'd'.
-	 * @return BitSet[] A list of keys for each round
-	 */
-	private static BitSet[] keyExpansion(StringBuilder keyStr, String type) {
-		//make sure keyStr is bitset of 64 size
-		//BitSet keyBlock64 = StringtoBitSet(keyStr.toString());
-//maybe do sanity check of parity, not done yet
-BitSet keyBlock64 = HextoBitSet("c82cea9ed93dfb8f");
-		BitSet[] keyBlock56 = key_64_to_2_28(keyBlock64);
-		BitSet[] keys = new BitSet[16];
-//printBitSet(keyBlock56[1]);
-		for(int i = 0; i < 16; i ++){
+    /**
+     * Generate key for each round based on encryption or decryption
+     * @param keyStr The key string
+     * @param type Encryption or Decryption. Values are 'e' or 'd'.
+     * @return BitSet[] A list of keys for each round
+     */
+    private static BitSet[] keyExpansion(StringBuilder keyStr, String type) {
+        //make sure keyStr is bitset of 64 size
+        //BitSet keyBlock64 = StringtoBitSet(keyStr.toString());
+        //maybe do sanity check of parity, not done yet
+        BitSet keyBlock64 = HextoBitSet("c82cea9ed93dfb8f");
+        BitSet[] keyBlock56 = key_64_to_2_28(keyBlock64);
+        BitSet[] keys = new BitSet[16];
+        for(int i = 0; i < 16; i ++){
 
-			//round key generation
-			LeftCircularShift(keyBlock56[0], rotations[i]);
-			LeftCircularShift(keyBlock56[1], rotations[i]);
-//printBitSet(keyBlock56[0]);
-//printBitSet(keyBlock56[1]);
-			BitSet roundKey = key_2_28_to_48(keyBlock56);
-			//mode
-printBitSet(roundKey,6);
-			if(type.equals("d"))
-				keys[i] = roundKey;
-			else if(type.equals("e"))
-				keys[15 - i] = roundKey;
-			else 	System.out.println("wrong mode, plz use e or d");
-		}
-		return keys;
-	}
-	/**
-	 * left shift block for movingBits, for 28 bit block only
-	 */
-	private static void LeftCircularShift(BitSet block, int movingBits){
-		BitSet buffer = new BitSet(movingBits);
-		buffer.clear();
-//printBitSet(block);
-		for(int i = 0; i < movingBits; i++){
-			if(block.get(i) == true)
-				buffer.set(i);
-		}
-		for(int i = movingBits; i < 28; i++){
-			block.set(i-movingBits,block.get(i));
-		}
-		for(int i = 28 - movingBits; i < 28; i++){
-			block.set(i,buffer.get(i-28+movingBits));
-		}
-//printBitSet(block);
-	}
+            //round key generation
+            LeftCircularShift(keyBlock56[0], rotations[i]);
+            LeftCircularShift(keyBlock56[1], rotations[i]);
+            BitSet roundKey = key_2_28_to_48(keyBlock56);
+            //mode
+            //printBitSet(roundKey,6);
+            if(type.equals("d"))
+                keys[i] = roundKey;
+            else if(type.equals("e"))
+                keys[15 - i] = roundKey;
+            else
+                System.out.println("wrong mode, plz use e or d");
+        }
+        return keys;
+    }
+    /**
+     * left shift block for movingBits, for 28 bit block only
+     */
+    private static void LeftCircularShift(BitSet block, int movingBits){
+        BitSet buffer = new BitSet(movingBits);
+        buffer.clear();
+        for(int i = 0; i < movingBits; i++){
+            if(block.get(i) == true)
+                buffer.set(i);
+        }
+        for(int i = movingBits; i < 28; i++){
+            block.set(i-movingBits,block.get(i));
+        }
+        for(int i = 28 - movingBits; i < 28; i++){
+            block.set(i,buffer.get(i-28+movingBits));
+        }
+    }
 
     /**
      * Print BitSet as 1s and 0s
@@ -470,54 +463,47 @@ printBitSet(roundKey,6);
         }
         System.out.print("\n");
     }
-	/**
-	 * key extraction used in key expansion
-	 */
-	private static BitSet[] key_64_to_2_28(BitSet keyBlock64){
-		BitSet[] keyBlock56 = new BitSet[2];
-		keyBlock56[0] = new BitSet(28);
-		keyBlock56[1] = new BitSet(28);
-		for(int i = 0; i < 4; i++){
-			boolean parity = true;
-			for(int j = 0; j < 7; j++){
+    /**
+     * key extraction used in key expansion
+     */
+    private static BitSet[] key_64_to_2_28(BitSet keyBlock64){
+        BitSet[] keyBlock56 = new BitSet[2];
+        keyBlock56[0] = new BitSet(28);
+        keyBlock56[1] = new BitSet(28);
+        for(int i = 0; i < 4; i++){
+            boolean parity = true;
+            for(int j = 0; j < 7; j++){
 
-				if(keyBlock64.get(PC1[i*7 + j] - 1) == true){
-//System.out.println((i*7 + j) + " " +(PC1[i*7 + j] - 1));
-					keyBlock56[0].set(i*7 + j);
-					parity = !parity;
-				}
-			}
-		}
-		for(int i = 4; i < 8; i++){
-			boolean parity = false;
-			for(int j = 0; j < 7; j++){
-//System.out.println(i+ " " +(i*7 + j));
-//System.out.println(i+ " " +PC1[i*7 + j]);
-				if(keyBlock64.get(PC1[i*7 + j] - 1) == true){
-					keyBlock56[1].set(i*7 + j - 28);
-				}
-			}
-		}
+                if(keyBlock64.get(PC1[i*7 + j] - 1) == true){
+                    keyBlock56[0].set(i*7 + j);
+                    parity = !parity;
+                }
+            }
+        }
+        for(int i = 4; i < 8; i++){
+            boolean parity = false;
+            for(int j = 0; j < 7; j++){
+                if(keyBlock64.get(PC1[i*7 + j] - 1) == true){
+                    keyBlock56[1].set(i*7 + j - 28);
+                }
+            }
+        }
 
-		return keyBlock56;
-	}
-	/**
-	 * key generation phase used in key expansion
-	 */
-	private static BitSet key_2_28_to_48(BitSet[] keyBlock56){
-		BitSet roundKey = new BitSet(56);
-//printBitSet(keyBlock56[0],7);
-//printBitSet(keyBlock56[1],7);
-		for(int i = 0; i < 48; i++){
-//System.out.println(PC2[i]);
-			if(PC2[i] > 28)
-				roundKey.set(i,keyBlock56[1].get(PC2[i]-29));
-			else roundKey.set(i,keyBlock56[0].get(PC2[i]-1));
-		}
-//printBitSet(roundKey,6);
-		return roundKey;
+        return keyBlock56;
+    }
+    /**
+     * key generation phase used in key expansion
+     */
+    private static BitSet key_2_28_to_48(BitSet[] keyBlock56){
+        BitSet roundKey = new BitSet(56);
+        for(int i = 0; i < 48; i++){
+            if(PC2[i] > 28)
+                roundKey.set(i,keyBlock56[1].get(PC2[i]-29));
+            else roundKey.set(i,keyBlock56[0].get(PC2[i]-1));
+        }
+        return roundKey;
 
-	}
+    }
 
     /**
      * Initial or Final Permutation
