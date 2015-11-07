16053fb8bbea2c690cc3177c3748501bbc10d0ef
Zhe Wang <zwang.contact@gmail.com>
2015-11-07 10:27:11 -0700

    update parameter parsing

--- RSA.java
+++ RSA.java
@@ -1,23 +1,27 @@
 import java.math.BigInteger;
-import java.security.SecureRandom
+import java.security.SecureRandom;
 import gnu.getopt.Getopt;
 
 
-public class RSA_skeleton {
+public class RSA{
 
     public static void main(String[] args){
 
+        StringBuilder kStr = new StringBuilder();
         StringBuilder bitSizeStr = new StringBuilder();
         StringBuilder nStr = new StringBuilder();
         StringBuilder dStr = new StringBuilder();
         StringBuilder eStr = new StringBuilder();
         StringBuilder m = new StringBuilder();
 
-        pcl(args, bitSizeStr, nStr, dStr, eStr,m);
+        pcl(args, kStr, bitSizeStr, nStr, dStr, eStr,m);
 
-        if(!bitSizeStr.toString().equalsIgnoreCase("")){
-            //This means you want to create a new key
-            genRSAkey(bitSizeStr);
+        if(kStr.toString().equalsIgnoreCase("k")) {
+            if(!bitSizeStr.toString().equalsIgnoreCase("")){
+                genRSAkey(Integer.parseInt(bitSizeStr.toString()));
+            } else {
+                genRSAkey(1024);
+            }
         }
 
         if(!eStr.toString().equalsIgnoreCase("")){
@@ -42,15 +46,18 @@ public class RSA_skeleton {
         // TODO Auto-generated method stub
     }
 
-    private static void genRSAkey(StringBuilder bitSizeStr) {
+    private static void genRSAkey(int bitSize) {
         // TODO Auto-generated method stub
+        System.out.println("generate key");
+        System.out.println(bitSize);
     }
 
 
     /**
      * This function Processes the Command Line Arguments.
      */
-    private static void pcl(String[] args, StringBuilder bitSizeStr,
+    private static void pcl(String[] args, StringBuilder kStr,
+            StringBuilder bitSizeStr,
             StringBuilder nStr, StringBuilder dStr, StringBuilder eStr,
             StringBuilder m) {
         /*
@@ -78,6 +85,7 @@ public class RSA_skeleton {
                     dStr.append(arg);
                     break;
                 case 'k':
+                    kStr.append('k');
                     break;
                 case 'b':
                     arg = g.getOptarg();
